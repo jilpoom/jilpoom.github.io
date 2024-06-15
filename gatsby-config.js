0ce0abf39c0dbd1afeb8590112c8a52d3493d884
Palladium <wjddyd94@naver.com>
2024-06-15 15:49:05 +0900

    03-Cookie,Session,JWT 시작 및 폰트 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,69 +8,82 @@
  * @type {import("gatsby").GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Palad Tech Blog`,
-    author: {
-      name: `Palad`,
-      summary: "Backend",
+    siteMetadata: {
+        title: `Palad Tech Blog`,
+        author: {
+            name: `Palad`,
+            summary: "Backend",
+        },
+        description: `Palad 기술 블로그`,
+        siteUrl: `https://jilpoom.github.io`,
+        social: {
+            github: `jilpoom`,
+        },
     },
-    description: `Palad 기술 블로그`,
-    siteUrl: `https://jilpoom.github.io`,
-    social: {
-      github: `jilpoom`,
-    },
-  },
-  plugins: [
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-autolink-headers`,
+    plugins: [
+        {
+            resolve: `gatsby-omni-font-loader`,
             options: {
-              className: `custom-class`,
-              maintainCase: false,
-              removeAccents: true,
-              elements: [`h1`, "h2"],
+                enableListener: true,
+                preconnect: [ `https://fonts.googleapis.com`, `https://fonts.gstatic.com` ],
+                web: [
+                    {
+                        name: `Noto Sans KR`,
+                        file: 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap',
+                    },
+                ],
             },
-          },
-          {
-            resolve: `gatsby-remark-images`,
+        },
+        `gatsby-plugin-image`,
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 630,
+                path: `${__dirname}/content/blog`,
+                name: `blog`,
             },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+                name: `images`,
+                path: `${__dirname}/src/images`,
             },
-          },
-          `gatsby-remark-prismjs`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-autolink-headers`,
+                        options: {
+                            className: `custom-class`,
+                            maintainCase: false,
+                            removeAccents: true,
+                            elements: [ `h1`, "h2" ],
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 630,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`,
+                        },
+                    },
+                    `gatsby-remark-prismjs`,
+                ],
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
           {
             site {
               siteMetadata {
@@ -82,20 +95,20 @@ module.exports = {
             }
           }
         `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map(node => {
-                return Object.assign({}, node.frontmatter, {
-                  description: node.excerpt,
-                  date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
-                })
-              })
-            },
-            query: `{
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.nodes.map(node => {
+                                return Object.assign({}, node.frontmatter, {
+                                    description: node.excerpt,
+                                    date: node.frontmatter.date,
+                                    url: site.siteMetadata.siteUrl + node.fields.slug,
+                                    guid: site.siteMetadata.siteUrl + node.fields.slug,
+                                    custom_elements: [ { "content:encoded": node.html } ],
+                                })
+                            })
+                        },
+                        query: `{
               allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
                 nodes {
                   excerpt
@@ -110,25 +123,25 @@ module.exports = {
                 }
               }
             }`,
-            output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-  ],
+                        output: "/rss.xml",
+                        title: "Gatsby Starter Blog RSS Feed",
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby Starter Blog`,
+                short_name: `Gatsby`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                // This will impact how browsers show your PWA/website
+                // https://css-tricks.com/meta-theme-color-and-trickery/
+                // theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+    ],
 }
